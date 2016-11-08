4ef1f34de5928e52279c0516ee3c78dfd11739ab
kangduan <duanea06606@gmail.com>
2016-11-08 10:21:38 +0000

    dfklj

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  get 'home/group'
-  get 'home/history'
-  get 'home/rental'
+  get 'home/about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
