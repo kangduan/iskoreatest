ee98bb857f8abb39dc670219074add0a9e304cbf
kangduan <duanea06606@gmail.com>
2016-11-10 09:40:26 +0000

    dfkjl

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
+require 'mailgun'
 class HomeController < ApplicationController
   def index
   end
@@ -6,8 +7,21 @@ class HomeController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @content = params[:content]
+    mg_client = Mailgun::Client.new("key-ac89645311d8534bb27750e824ad715a")
 
-    redirect_to : "/"
+    message_params =  {
+                   from: @email,
+                   to:   'duanea0606@gmail.com',
+                   subject: @name,
+                   text:    @content
+                  }
+
+                  result = mg_client.send_message('sandbox165c26fd72294c21b95418c845d11557.mailgun.org', message_params).to_h!
+
+                  message_id = result['id']
+                  message = result['message']
+
+                  redirect_to : "/"
 
   end
 
